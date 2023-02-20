f53eec48ad2eb6d4b939708bac422b984f63dfc5
June Dougherty <zachharlandougherty@gmail.com>
2023-02-20 16:10:02 -0500

    done and i hope this uploads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo.interface'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularToDo_Lab';
+  title = 'To Do List';
+
+  todos: Todo[] = [
+    { task: 'Feed Cats', completed: false },
+    { task: 'Buy groceries', completed: false },
+    { task: 'Clean Litter Box', completed: true },
+    { task: 'Rake Leaves', completed: true }
+  ];
+  newTask: string = "";
+
+  addTask() {
+    if (this.newTask.trim().length === 0){
+      return;
+    }
+    const newTodo: Todo = {
+      task: this.newTask,
+      completed: false
+    };
+    this.todos.push(newTodo);
+    this.newTask = ""
+  }
+  completeTask(task: Todo) {
+    task.completed = true;
+  }
 }
